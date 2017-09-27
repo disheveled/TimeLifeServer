37adbb141b2f23e744b9112a20ddb13ad07eafa1
Нечесанова Елена <disheveled.lee@gmail.com>
2017-09-27 17:10:02 +0300

    dispose sponsor

---
+++ TimeLifeServer/DisposingSponsor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Runtime.Remoting.Lifetime;
+
+namespace TimeLifeServer
+{
+    class DisposingSponsor : ISponsor
+    {
+        private IDisposable mManagedObj;
+        public DisposingSponsor(IDisposable managedObj)
+        {
+            mManagedObj = managedObj;
+        }
+        public TimeSpan Renewal(ILease leaseInfo)
+        {
+            mManagedObj.Dispose();
+            return TimeSpan.FromSeconds(0);
+        }
+    }
+}
